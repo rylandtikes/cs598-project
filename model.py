96a8e8f19c8db01bb5932a002ac56d452070fb1e
Sean Enright <seanre2@illinois.edu>
2023-04-29 22:37:31 -0700

    GraphLayer refactoring; initialize out_attn

--- model.py
+++ model.py
@@ -52,75 +52,122 @@ class GraphLayer(nn.Module):
         self.concat = concat
         self.num_of_nodes = num_of_nodes
         self.num_of_heads = num_of_heads
-        self.W = clones(nn.Linear(in_features, hidden_features), num_of_heads)
-        self.a = clone_params(nn.Parameter(torch.rand(size=(1, 2 * hidden_features)), requires_grad=True), num_of_heads)
-        self.ffn = nn.Sequential(
-            nn.Linear(out_features, out_features),
-            nn.ReLU()
-        )
-        if not concat:
-            self.V = nn.Linear(hidden_features, out_features)
-        else:
-            self.V = nn.Linear(num_of_heads * hidden_features, out_features)
-        self.dropout = nn.Dropout(dropout)
+        # Attention
+        # Linear layers for multi-head attention
+        self.W = clones(
+            nn.Linear(in_features, hidden_features),
+            num_of_heads
+            )
+        # Parameters for multi-head attention
+        self.a = clone_params(
+            nn.Parameter(torch.rand(size=(1, 2 * hidden_features)), requires_grad=True),
+            num_of_heads
+            )
         self.leakyrelu = nn.LeakyReLU(self.alpha)
+
+        # FFN applied after attention
+        self.linear = nn.Linear(hidden_features, out_features)
         if concat:
-            self.norm = LayerNorm(hidden_features)
+            self.ffn = nn.Sequential(
+                nn.Dropout(dropout),
+                LayerNorm(hidden_features),
+                nn.ELU()
+                # linear is not present here in official code (should it be?)
+            )
         else:
-            self.norm = LayerNorm(hidden_features)
+            self.ffn = nn.Sequential(
+                nn.Dropout(dropout),
+                LayerNorm(hidden_features),
+                nn.ReLU(),
+                self.linear
+            ) 
 
-    def initialize(self):
+    def initialize(self, init_method=nn.init.xavier_normal_):
+        """Set initial weights in model.
+        """
         for i in range(len(self.W)):
-            nn.init.xavier_normal_(self.W[i].weight.data)
+            init_method(self.W[i].weight.data)
         for i in range(len(self.a)):
-            nn.init.xavier_normal_(self.a[i].data)
+            init_method(self.a[i].data)
         if not self.concat:
-            nn.init.xavier_normal_(self.V.weight.data)
-            nn.init.xavier_normal_(self.out_layer.weight.data)
+            init_method(self.linear.weight.data)
 
-    def attention(self, linear, a, N, data, edge):
+    def attention(self, linear, a, data, edges):
+        """Updates representation by graph propagation with self-attention.
+
+            Uses 'Method 2': the inner product of a learnable vector and the concatenation of two
+            relevant representations.
+
+        Args:
+            linear (torch.Module): linear layer
+            a (torch.Parameter): parameters of attention
+            data (torch.Tensor): representation of EHR codes,
+                                 of shape (self.num_of_nodes, embedding dim)
+            edge (torch.Tensor): edge connectivity vector for observed codes, of shape (2, v_obs^2)
+
+        Returns:
+            torch.Tensor: updated representation of EHR codes after attenention from graph
+        """
         data = linear(data).unsqueeze(0)
-        assert not torch.isnan(data).any()
-        # edge: 2*D x E
-        h = torch.cat((data[:, edge[0, :], :], data[:, edge[1, :], :]), dim=0)
+        # h: self.num_of_nodes x out
+        h = torch.cat(
+            (data[:, edges[0, :], :],
+             data[:, edges[1, :], :]), dim=0)
         data = data.squeeze(0)
-        # h: N x out
-        assert not torch.isnan(h).any()
         # edge_h: 2*D x E
-        edge_h = torch.cat((h[0, :, :], h[1, :, :]), dim=1).transpose(0, 1)
-        # edge: 2*D x E
-        edge_e = torch.exp(self.leakyrelu(a.mm(edge_h).squeeze()) / np.sqrt(self.hidden_features * self.num_of_heads))
-        assert not torch.isnan(edge_e).any()
+        edge_h = torch.cat(
+            (h[0, :, :],
+             h[1, :, :]), dim=1).transpose(0, 1)
+        # d_h: dimensionality of embedding dimension
+        d_h = np.sqrt(self.hidden_features * self.num_of_heads)
         # edge_e: E
-        edge_e = torch.sparse_coo_tensor(edge.to(device), edge_e.to(device), torch.Size([N, N]))
-        e_rowsum = torch.sparse.mm(edge_e, torch.ones(size=(N, 1)).to(device))
+        edge_e = torch.exp(self.leakyrelu(a.mm(edge_h).squeeze()) / d_h)
+        edge_e = torch.sparse_coo_tensor(
+            edges.to(device),
+            edge_e.to(device),
+            torch.Size([self.num_of_nodes, self.num_of_nodes])
+            )
+        e_rowsum = torch.sparse.mm(edge_e, torch.ones(size=(self.num_of_nodes, 1)).to(device))
         # e_rowsum: N x 1
         row_check = (e_rowsum == 0)
         e_rowsum[row_check] = 1
         zero_idx = row_check.nonzero()[:, 0]
-        edge_e = edge_e.add(
-            torch.sparse.FloatTensor(zero_idx.repeat(2, 1), torch.ones(len(zero_idx)).to(device), torch.Size([N, N])))
+        edge_e = edge_e.add(torch.sparse.FloatTensor(
+                               zero_idx.repeat(2, 1),
+                               torch.ones(len(zero_idx)).to(device),
+                               torch.Size([self.num_of_nodes, self.num_of_nodes])
+                               ))
         # edge_e: E
         h_prime = torch.sparse.mm(edge_e, data)
-        assert not torch.isnan(h_prime).any()
         # h_prime: N x out
         h_prime.div_(e_rowsum)
-        # h_prime: N x out
-        assert not torch.isnan(h_prime).any()
         return h_prime
 
-    def forward(self, edge, data=None):
-        N = self.num_of_nodes
+    def forward(self, edges, data=None):
+        """Uses fully connected graph to update representation of EHR codes via self-attention.
+
+        Args:
+            edge (torch.Tensor): edge connectivity vector for observed codes, of shape (2, v_obs^2)
+            data (torch.Tensor): representation of EHR codes,
+                                 of shape (self.num_of_nodes, embedding dim)
+
+        Returns:
+            torch.Tensor: updated representation of EHR codes
+        """
+        # TODO: separate forward for multi-head?
+
+        # If using multi-head attention (self.num_of_heads > 1), compute attention coeffs multiple
+        # times. If is input layer (self.concat), aggregate coeffs by concatenation.
+        attn_list = []
+        for w, a in zip(self.W, self.a):
+            attn_out = self.attention(w, a, data, edges)
+            attn_list.append(attn_out)
         if self.concat:
-            h_prime = torch.cat([self.attention(l, a, N, data, edge) for l, a in zip(self.W, self.a)], dim=1)
+            h_prime = torch.cat(attn_list, dim=1)
         else:
-            h_prime = torch.stack([self.attention(l, a, N, data, edge) for l, a in zip(self.W, self.a)], dim=0).mean(
-                dim=0)
-        h_prime = self.dropout(h_prime)
-        if self.concat:
-            return F.elu(self.norm(h_prime))
-        else:
-            return self.V(F.relu(self.norm(h_prime)))
+            h_prime = torch.stack(attn_list, dim=0).mean(dim=0)
+        # FFN
+        return self.ffn(h_prime)
 
 
 class VariationalGNN(nn.Module):
@@ -156,16 +203,20 @@ class VariationalGNN(nn.Module):
             self.features_ffn = nn.Sequential(
                 nn.Linear(excluded_features, out_features // 2),
                 nn.ReLU(),
-                nn.Dropout(dropout))
+                nn.Dropout(dropout)
+                )
         self.out_layer = nn.Sequential(
+            nn.ReLU(),
             nn.Linear(linear_out_input, out_features),
             nn.ReLU(),
             nn.Dropout(dropout),
-            nn.Linear(out_features, 1))
+            nn.Linear(out_features, 1)
+            )
         
         # Initialize encoder graph layers
         for i in range(n_layers):
             self.in_att[i].initialize()
+        self.out_att.initialize() # Out attn intialization wasn't being performed previously
 
     @staticmethod
     def make_fc_graph_edges(nodes):
@@ -195,24 +246,24 @@ class VariationalGNN(nn.Module):
             (torch.Tensor, torch.Tensor): input and output graph connectivity in COO format
         """
         n = codes.size()[0]
-        nonzero = codes.nonzero() # nonzero: indices with non-zero codes, of shape (num_nonzero, 1)
-        if nonzero.size()[0] == 0:
+        observed = codes.nonzero() # observed EHR codes, of shape (num_nonzero, 1)
+        if observed.size()[0] == 0:
             return torch.LongTensor([[0], [0]]), torch.LongTensor([[n + 1], [n + 1]])
         if self.training:
-            # Exclude non-zero codes with 0.05 probability
-            mask = torch.rand(nonzero.size()[0])
+            # Exclude observed codes with 0.05 probability
+            mask = torch.rand(observed.size()[0])
             mask = mask > 0.05
-            nonzero = nonzero[mask]
-            if nonzero.size()[0] == 0:
+            observed = observed[mask]
+            if observed.size()[0] == 0:
                 return torch.LongTensor([[0], [0]]), torch.LongTensor([[n + 1], [n + 1]])
         # Input edges
         # Nodes, of shape (1, n), incremented by 1
-        nonzero = nonzero.transpose(0, 1) + 1
-        input_edges = self.make_fc_graph_edges(nonzero)
+        observed = observed.transpose(0, 1) + 1
+        input_edges = self.make_fc_graph_edges(observed)
         # Output edges
         # Adds a node at the end with value n + 1. Will act as prediction node.
-        nonzero = torch.cat((nonzero, torch.LongTensor([[n + 1]]).to(device)), dim=1)
-        output_edges = self.make_fc_graph_edges(nonzero)
+        observed = torch.cat((observed, torch.LongTensor([[n + 1]]).to(device)), dim=1)
+        output_edges = self.make_fc_graph_edges(observed)
 
         return input_edges.to(device), output_edges.to(device)
 
@@ -321,7 +372,7 @@ class VariationalGNN(nn.Module):
                 included_batch.append(out)
                 kld_batch.append(kld)
             out_batch = torch.cat((torch.stack(excluded_batch), torch.stack(included_batch)),
-                                   dim=1)
-        logits = self.out_layer(F.relu(out_batch))
+                                  dim=1)
+        logits = self.out_layer(out_batch)
         kld_sum = torch.sum(torch.stack(kld_batch))
         return logits, kld_sum
